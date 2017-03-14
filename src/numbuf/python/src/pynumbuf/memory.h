a759ad8ae4c2518d72c3037e263309b87472746b
Alexey Tumanov <atumanov@gmail.com>
2017-03-14 02:59:29 -0700

    numbuf: add threadpool for memcopy

--- src/numbuf/python/src/pynumbuf/memory.h
+++ src/numbuf/python/src/pynumbuf/memory.h
@@ -7,6 +7,11 @@
 #include <vector>
 #include <string>
 #include <thread>
+
+#define NUMTHREADS 8
+#define MB  (1<<20)
+#define KB  (1<<10)
+
 using namespace std;
 
 namespace numbuf {
@@ -17,7 +22,7 @@ class FixedBufferStream : public arrow::io::OutputStream,
   virtual ~FixedBufferStream() {}
 
   explicit FixedBufferStream(uint8_t* data, int64_t nbytes)
-      : data_(data), position_(0), size_(nbytes) {}
+      : data_(data), position_(0), size_(nbytes), threadpool_(NUMTHREADS) {}
 
   arrow::Status Read(int64_t nbytes, std::shared_ptr<arrow::Buffer>* out) override {
     DCHECK(out);
@@ -50,10 +55,10 @@ class FixedBufferStream : public arrow::io::OutputStream,
     DCHECK(position_ + nbytes <= size_) << "position: " << position_
                                         << " nbytes: " << nbytes << "size: " << size_;
     uint8_t* dst = data_ + position_;
-    if (nbytes >= (1<<20)) {
-      memcopy_block_aligned(dst, data, nbytes, false);
+    if (nbytes >= MB) {
+      memcopy_block_aligned(dst, data, nbytes);
     } else {
-      memcopy_vanilla(dst, data, nbytes, false);
+      memcpy(dst, data, nbytes);
     }
     position_ += nbytes;
     return arrow::Status::OK();
@@ -70,33 +75,32 @@ class FixedBufferStream : public arrow::io::OutputStream,
   uint8_t* data_;
   int64_t position_;
   int64_t size_;
+  std::vector<std::thread> threadpool_;
 
-void memcopy_vanilla(uint8_t *dst, const uint8_t *src, uint64_t nbytes, bool timeit) {
-  struct timeval tv1, tv2;
-  double elapsed = 0;
+  void memcopy_vanilla(uint8_t *dst, const uint8_t *src, uint64_t nbytes,
+                       bool timeit) {
+    struct timeval tv1, tv2;
+    double elapsed = 0;
 
-  if (timeit) {
-    gettimeofday(&tv1, NULL);
+    if (timeit) {
+      gettimeofday(&tv1, NULL);
+    }
+    memcpy(dst, src, nbytes);
+    if (timeit) {
+      gettimeofday(&tv2, NULL);
+      elapsed = ((tv2.tv_sec - tv1.tv_sec) * 1000000
+          + (tv2.tv_usec - tv1.tv_usec)) / 1000000.0;
+      printf("copied %llu bytes in time = %8.4f MBps=%8.4f\n", nbytes, elapsed,
+          nbytes / ((1 << 20) * elapsed));
+    }
   }
-  memcpy(dst, src, nbytes);
-  if (timeit) {
-    gettimeofday(&tv2, NULL);
-    elapsed = ((tv2.tv_sec - tv1.tv_sec)*1000000 + (tv2.tv_usec - tv1.tv_usec))/1000000.0;
-    printf("copied %llu bytes in time = %8.4f MBps=%8.4f\n",
-           nbytes, elapsed, nbytes/((1<<20)*elapsed));
-  }
-}
 
-int memcopy_block_aligned(uint8_t *dst, const uint8_t *src, uint64_t nbytes, bool timeit)
-{
-#ifndef NUMTHREADS
-#define NUMTHREADS 8
-#endif
+int memcopy_aligned(uint8_t *dst, const uint8_t *src, uint64_t nbytes,
+                    uint64_t blocksz, bool timeit) {
   int rv = 0;
   struct timeval tv1, tv2;
   double elapsed = 0;
-  const uint64_t numthreads = NUMTHREADS;
-  const uint64_t blocksz = getpagesize();
+  const uint64_t numthreads = NUMTHREADS-2;
   const char *srcbp = (char *)(((uint64_t)src + blocksz-1) & ~(blocksz-1));
   char *srcep = (char *)(((uint64_t)src + nbytes) & ~(blocksz-1));
   const uint64_t numblocks = (((uint64_t)srcep - (uint64_t)srcbp)) / blocksz;
@@ -113,21 +117,24 @@ int memcopy_block_aligned(uint8_t *dst, const uint8_t *src, uint64_t nbytes, boo
   // chunksz = k*blocksz => data == | prefix | numthreads*chunksz | suffix |
   // Each thread gets a "chunk" of k blocks, except prefix and suffix threads.
 
-  std::vector<std::thread> threads;
+//  std::vector<std::thread> threads;
   // Start the prefix thread.
   if (timeit) {
     gettimeofday(&tv1, NULL);
   }
-  threads.push_back(std::thread(memcpy, dst, src, prefix));
-  for (int i = 1; i <= numthreads; i++) {
-    threads.push_back(std::thread(
-        memcpy, dst+prefix+(i-1)*chunksz, srcbp + (i-1)*chunksz, chunksz));
+  // Start all the threads first. Then handle leftovers while threads run.
+  for (int i = 0; i < numthreads; i++) {
+    threadpool_[i] =
+        std::thread(memcpy, dst+prefix+i*chunksz, srcbp + i*chunksz, chunksz);
   }
-  threads.push_back(std::thread(memcpy, dstep, srcep, suffix));
+//  threads.push_back(std::thread(memcpy, dstep, srcep, suffix));
+  memcpy(dst, src, prefix);
+  memcpy(dstep, srcep, suffix);
 
-  // Join the memcpy threads.
-  for (auto &t: threads) {
-    t.join();
+  for (auto &t: threadpool_) {
+    if (t.joinable()) {
+      t.join();
+    }
   }
   if (timeit) {
     gettimeofday(&tv2, NULL);
@@ -136,6 +143,14 @@ int memcopy_block_aligned(uint8_t *dst, const uint8_t *src, uint64_t nbytes, boo
            nbytes, elapsed, nbytes/((1<<20)*elapsed));
   }
   return rv;
+}
+
+int memcopy_block_aligned(uint8_t *dst, const uint8_t *src, uint64_t nbytes) {
+  return memcopy_aligned(dst, src, nbytes, 64, false);
+}
+
+int memcopy_page_aligned(uint8_t *dst, const uint8_t *src, uint64_t nbytes) {
+  return memcopy_aligned(dst, src, nbytes, getpagesize(), false);
 }
 
 };
