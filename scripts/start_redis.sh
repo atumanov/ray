d421ada3a307812844473ba2bbf1e5e35d9b0445
Alexey Tumanov <atumanov@gmail.com>
2017-04-14 04:45:48 +0000

    start redis script

---
+++ scripts/start_redis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+RAY_HOME=/home/ubuntu/ray
+${RAY_HOME}/python/ray/core/src/common/thirdparty/redis/src/redis-server \
+    --port 6379 \
+    --loadmodule ${RAY_HOME}/python/ray/core/src/common/redis_module/libray_redis_module.so > /dev/null 2> /dev/null &
+sleep 1
+redis-cli -p 6379 config set protected-mode no
