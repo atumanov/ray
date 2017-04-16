e4933aafb52a66ce04e473d757dd4b824f188745
Alexey Tumanov <atumanov@gmail.com>
2017-04-16 08:06:33 +0000

    building on the centos ami

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RAY_HOME=/home/ubuntu/ray
+RAY_HOME=$HOME/ray
 ${RAY_HOME}/python/ray/core/src/common/thirdparty/redis/src/redis-server \
     --port 6379 \
     --loadmodule ${RAY_HOME}/python/ray/core/src/common/redis_module/libray_redis_module.so > /dev/null 2> /dev/null &
